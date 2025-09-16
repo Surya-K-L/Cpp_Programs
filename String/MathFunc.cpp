c1248a5e9a7ac87374ceebf6ab91c7fac2a48535
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2025-09-16 07:01:23 +0530

    Create MathFunc.cpp

---
+++ String/MathFunc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+   cout<<max(2,6)<<"\n";
+   cout<<min(2,6)<<"\n";
+   cout<<sqrt(9)<<"\n";
+   cout<<round(2.3)<<"\n";
+   cout<<log(3);
+}
